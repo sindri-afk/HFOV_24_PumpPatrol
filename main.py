574754e2a778aaef6096f711663e152613ef46a8
ollisam <142885945+ollisam@users.noreply.github.com>
2024-12-03 13:04:34 +0000

    display finished

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from SignInUp.register_user import register_user
+import time
+from SignInUp.controllers.auth_controller import AuthController
+from display import display_menu
+
+user = None
+auth_controller = AuthController()
+
+def sign_up():
+    auth_controller.sign_up()  # Handled by AuthController
+    time.sleep(1)
+    main()
+
+def sign_in():
+    global user
+    user = auth_controller.sign_in()  # Handled by AuthController
+    if user:
+        print(f"Welcome {user.username}!")
+        time.sleep(1)
+    else:
+        time.sleep(1)
+        main()
 
 
 def main():
-    user = register_user()
-    while True:
-        print("\n1. View Memberships\n2. Log Out")
-        user_choice = input("Select an option: ")
+    # registering user
+    menu_options = {
+        1: ("Sign Up", sign_up),
+        2: ("Sign In", sign_in),
+        3: ("Exit", None),
+    }
+    display_menu("City Gym Hub", menu_options)
 
-        if user_choice == "1":
-            print(f"Memberships for {user.username}")  # Show user profile
-        elif user_choice == "2":
-            print(f"Goodbye, {user.username}!")
-            break  # Breaks the inner loop and returns to main menu
-        else:
-            print("Invalid option. Try again.")
+    # user main menu
+    if user:
+        menu_options = {
+            1: ("View Memberships", None),
+            2: ("Exit", None),
+        }
+        display_menu("City Gym Hub", menu_options)
+
+    
 
 if __name__ == "__main__":
     main()
