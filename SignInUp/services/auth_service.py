c980b56e380319e286e7326b0efc01ff860de093
Benjamín Ragnarsson <benjarag7@outlook.com>
2024-12-09 11:19:43 +0000

    bug fix

--- SignInUp/services/auth_service.py
+++ SignInUp/services/auth_service.py
@@ -10,7 +10,7 @@ class AuthService:
         if self.user_repo.find_by_username(username):
             raise ValueError("Username already exists!")
         hashed_password = hash_password(password)
-        new_user = User(user_id=len(self.user_repo.load_users()) + 1, username=username, password_hash=hashed_password, membership_id=None, trainer=trainer)
+        new_user = User(user_id=len(self.user_repo.load_users()) + 1, username=username, password_hash=hashed_password, membership_id=0, trainer=trainer)
         self.user_repo.add_user(new_user)
         return new_user
 
